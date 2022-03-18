a116c9568583453aadc6bebdbd4601f3da8b2e45
Aryan Singh <aryansingh6011@gmail.com>
2022-03-18 12:51:15 +0530

    2nd Page Design made

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Email Button
 const emailBtn = document.getElementsByClassName('one')[0]; //Indexing the arry 
 emailBtn.addEventListener('click', function() {
-    location.href = "";
+    window.open("https://twitter.com/ItsApexAryan", "_blank");
 });
 
 // linkedin Button
@@ -25,19 +25,19 @@ githubBtn.addEventListener('click', function() {
 
 // Side Nav Toogle Buttons
 const items = document.querySelectorAll('.menu-item');
-   
-// on click
-  for (let item of items) {
-      if (item.querySelector(".sub-menu")) {
-        item.addEventListener("click", function() {
-            item.className = 'sub-menu-active';
-        });
-      }
 
-      if (item.querySelector(".sub-menu-active")) {
-        item.addEventListener("click", function() {
-            console.log('lk')
-            item.className = 'sub-menu';
-        });
-      }   
-  }
+// on click
+function toggleItem() {
+    if (this.classList.contains("sub-menu-active")) {
+        this.classList.remove("sub-menu-active");
+    } else {
+        this.classList.remove();
+        this.classList.add("sub-menu-active");
+    }
+}
+
+for (let item of items) {
+    if (item.querySelector(".sub-menu")) {
+        item.addEventListener("click", toggleItem, false);
+    }   
+}
